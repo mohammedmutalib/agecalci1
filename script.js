b53b723c7ff4df416f01e21287731180d4d68039
mohammedmutalib <125193976+mohammedmutalib@users.noreply.github.com>
2023-06-25 19:33:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculate() {
+    setInterval(() => {
+      //Input Birth date
+      let birthdate = new Date(document.getElementById('birthdate').value);
+  
+      // getTime : returns the number of milliseconds since January 1, 1970
+      let now = new Date();
+      let ageInMs = now.getTime() - birthdate.getTime();
+  
+      let ageInS = ageInMs / 1000; // to seconds
+      let ageInMins = ageInS / 60; // to minutes
+      let ageInHrs = ageInMins / 60; // to Hours
+      let ageInDays = ageInHrs / 24; // to days
+      let ageInMonths = ageInDays / 30.4375; // to months
+      let ageInYears = ageInMonths / 12; // to years
+  
+      document.querySelector('#years').innerHTML = Math.floor(ageInYears);
+      document.querySelector('#months').innerHTML = Math.floor(ageInMonths % 12);
+      document.querySelector('#days').innerHTML = Math.floor(ageInDays % 30.4375);
+      
+    }, 1000);
+  }
+  
+  function reset() {
+    window.location.reload();
+  }
